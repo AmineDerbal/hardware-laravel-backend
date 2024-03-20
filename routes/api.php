ca4d1a15b938b61839bca4d9ad05bb381401ada7
AmineDerbal <aminedoran31@gmail.com>
2024-03-20 14:12:09 +0100

    Create routes for users

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\UserController;
 
 
 /*
@@ -30,3 +31,5 @@ Route::controller(RegisterController::class)->group(function () {
 Route::controller(LoginController::class)->group(function () {
     Route::post('auth/login', 'login');
 });
+
+Route::resource('users', \App\Http\Controllers\Api\UserController::class)->except(['update'])->middleware(['auth:api', 'admin.check']);
